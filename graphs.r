97cf933408f25750803ff1d34ca0b557ce0d6293
Alexander Rehnman <privat@alexanderrehnman.se>
2018-12-31 15:01:45 +0100

    Fixed some bugs.

--- graphs.r
+++ graphs.r
@@ -1,17 +1,17 @@
-threads <- read.csv('threads.csv',header=TRUE,sep=";")
+threads <- read.csv('threads.csv',header=TRUE,sep=";",quote="")
 
 attach(threads)
 
 png("tradar_per_timme.png",width=1920,height=1080)
-barplot(table(CreationHour),main=sprintf("Skapade trådar per timme på dygnet (diagram skapat %s)",Sys.time()),col="#fd2860",ylim=c(0,40))
+barplot(table(CreationHour),main=sprintf("Skapade trådar per timme på dygnet (diagram skapat %s)",Sys.time()),col="#fd2860")
 dev.off()
 
 png("tradar_per_datum.png",width=1920,height=1080)
-barplot(table(CreationDate),main=sprintf("Skapade trådar per datum (diagram skapat %s)",Sys.time()),col="#fd2860",ylim=c(0,35))
+barplot(table(CreationDate),main=sprintf("Skapade trådar per datum (diagram skapat %s)",Sys.time()),col="#fd2860")
 dev.off()
 
 png("tradar_per_anvandare.png",width=1920,height=1080)
-barplot(sort(table(Creator),decreasing=TRUE)[1:15],main=sprintf("Skapade trådar per användare (topp 15) (diagram skapat %s)",Sys.time()),col="#fd2860",ylim=c(0,60))
+barplot(sort(table(Creator),decreasing=TRUE)[1:15],main=sprintf("Skapade trådar per användare (topp 15) (diagram skapat %s)",Sys.time()),col="#fd2860")
 dev.off()
 
 detach(threads)
@@ -28,7 +28,7 @@ attach(topfifteen)
 
 png("topp_15_tradar_utan_bsl.png",width=1920,height=1080)
 par(mar=c(25.1,4.1,4.1,2.1),las=2)
-barplot(NumberOfReplies,names.arg=Title,main=sprintf("Topp 15 trådar efter inlägg (förutom Bullshit Lounge) (diagram skapat %s)",Sys.time()),col="#fd2860",ylim=c(0,300))
+barplot(NumberOfReplies,names.arg=Title,main=sprintf("Topp 15 trådar efter inlägg (förutom Bullshit Lounge) (diagram skapat %s)",Sys.time()),col="#fd2860")
 dev.off()
 
 detach(topfifteen)
